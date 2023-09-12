fa62aaf43c0c0ff0c1547d25defdce89555ecf71
Benjamin Davis <davis.benjamin41902@gmail.com>
2023-09-11 22:00:04 -0400

    setup project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<div class="flex flex-row gap-4 justify-center">
+	<a href="/hooks" class="card variant-ghost-primary p-4 hover:variant-ghost-secondary">
+		<h1 class="text-lg font-semibold">Hooks 🪝</h1>
+	</a>
+	<a href="/view" class="card variant-ghost-primary p-4 hover:variant-ghost-secondary">
+		<h1 class="text-lg font-semibold">View Transitions 🪟</h1>
+	</a>
+	<a href="/animations" class="card variant-ghost-primary p-4 hover:variant-ghost-secondary">
+		<h1 class="text-lg font-semibold">Animations 📲</h1>
+	</a>
+	<a href="/bun" class="card variant-ghost-primary p-4 hover:variant-ghost-secondary">
+		<h1 class="text-lg font-semibold">Bun 🚀</h1>
+	</a>
+</div>
