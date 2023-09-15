34c48749ab852876c5565d42aff74202d8bc5e9c
Benjamin Davis <davis.benjamin41902@gmail.com>
2023-09-15 14:27:58 -0400

    removed un-needed style tag from root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -111,37 +111,3 @@
 		</div>
 	</footer>
 </div>
-
-<style>
-	@keyframes fade-in {
-		from {
-			opacity: 0;
-		}
-	}
-
-	@keyframes fade-out {
-		to {
-			opacity: 0;
-		}
-	}
-
-	@keyframes slide-from-right {
-		from {
-			transform: translateX(20px);
-		}
-	}
-
-	@keyframes slide-to-left {
-		to {
-			transform: translateX(-20px);
-		}
-	}
-
-	/* :root::view-transition-old(root) {
-		animation: 90ms cubic-bezier(0.4, 0, 1, 1) both fade-out;
-	}
-
-	:root::view-transition-new(root) {
-		animation: 90ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in;
-	} */
-</style>
